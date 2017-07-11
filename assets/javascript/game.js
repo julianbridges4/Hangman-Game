17d9e5c9309f46f5875b0653b1219515e105a155
Julian K. Bridges <julianbridges01@gmail.com>
2017-07-11 00:24:52 -0400

    updated javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,70 +14,60 @@ losses = document.getElementById("losses");
 guessNumber = document.getElementById("guess-number");
 
 function newWord() {
-		word = words[Math.floor(Math.random() * words.length)];
-		wordUnderscore = "";
-		alert(word);
-	for (i = 0; i < word.length; i++) {
-		wordUnderscore += "_";
-	}
+    word = words[Math.floor(Math.random() * words.length)];
+    wordUnderscore = "";
+    for (i = 0; i < word.length; i++) {
+        wordUnderscore += "_";
+    }
 
-	document.getElementById("word").innerHTML = wordUnderscore;
+    document.getElementById("word").innerHTML = wordUnderscore;
 
 }
 
 function reset() {
-	newWord();
-	guessNumber.innerHTML = 8;
-	guessLetters = "";
+    newWord();
+    guessNumber.innerHTML = 8;
+    guessLetters = "";
 }
 
 
-window.onload = function () {
-	guessNumber.innerHTML = 8;
-	wins.innerHTML = 0;
-	losses.innerHTML = 0;
-	newWord();
-	
+window.onload = function() {
+    guessNumber.innerHTML = 8;
+    wins.innerHTML = 0;
+    losses.innerHTML = 0;
+    newWord();
+
 }
 
 document.onkeyup = function(event) {
-	guessLetter = event.key;
-	letterIndex = [];
-	// if (guessLetters.indexOf(guessLetter) === -1) {
-	// 	guessLetters += guessLetter;
-	// 	document.getElementById("guess-letters").innerHTML = guessLetters;
-	// }
-	if (word.indexOf(guessLetter) === -1 && guessLetters.indexOf(guessLetter) === -1) {
-		guessNumber.innerHTML --;
-		if (guessLetters.indexOf(guessLetter) === -1) {
-		guessLetters += guessLetter;
-		document.getElementById("guess-letters").innerHTML = guessLetters;
-	}
-		if (guessNumber.innerHTML == 0) {
-		losses.innerHTML ++;
-		reset();
-		}
-	}
-	for (i = 0; i < word.length; i++) {
-		if (word[i] === guessLetter) {
-			letterIndex.push(i);
-			console.log(letterIndex);
-		}
-	}
-	console.log(letterIndex);
-	for (j = 0; j < letterIndex.length; j++) {
-		wordUnderscore = wordUnderscore.substring(0, letterIndex[j]) + word[letterIndex[j]] + wordUnderscore.substring(letterIndex[j] + 1);
-		console.log(wordUnderscore);
-		
-	}
-	document.getElementById("word").innerHTML = wordUnderscore;
-	if (wordUnderscore === word) {
-		wins.innerHTML ++;
-		reset();
-	}
-	// if (guessNumber.innerHTML == 0) {
-	// 	losses.innerHTML ++;
-	// 	reset();
-	// }
+    guessLetter = event.key;
+    letterIndex = [];
 
+    if (word.indexOf(guessLetter) === -1 && guessLetters.indexOf(guessLetter) === -1) {
+        guessNumber.innerHTML--;
+        if (guessLetters.indexOf(guessLetter) === -1) {
+            guessLetters += guessLetter;
+            document.getElementById("guess-letters").innerHTML = guessLetters;
+        }
+        if (guessNumber.innerHTML == 0) {
+            losses.innerHTML++;
+            reset();
+        }
+    }
+    for (i = 0; i < word.length; i++) {
+        if (word[i] === guessLetter) {
+            letterIndex.push(i);
+        }
+    }
+    console.log(letterIndex);
+    for (j = 0; j < letterIndex.length; j++) {
+        wordUnderscore = wordUnderscore.substring(0, letterIndex[j]) + word[letterIndex[j]] + wordUnderscore.substring(letterIndex[j] + 1);
+        console.log(wordUnderscore);
+
+    }
+    document.getElementById("word").innerHTML = wordUnderscore;
+    if (wordUnderscore === word) {
+        wins.innerHTML++;
+        reset();
+    }
 }
